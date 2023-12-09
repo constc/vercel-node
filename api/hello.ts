a4d169de2f36c9f2dfac0a08e6cf6115bae0a90f
Const <78634617+constc@users.noreply.github.com>
2023-12-09 08:56:08 +0300

    speed-insights

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
