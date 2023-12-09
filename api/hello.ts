6d6a59ff7a0f589b8247c0675afabfebe03ef1e9
Const <78634617+constc@users.noreply.github.com>
2023-12-09 09:45:04 +0300

    j

--- api/hello.ts
+++ api/hello.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { sql } from '@vercel/postgres';
 import { injectSpeedInsights } from '@vercel/speed-insights'
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async (req: VercelRequest, res: VercelResponse) => {
   const { rows } = await sql`SELECT * FROM tbl;`;
   return res.json(rows)
 }
